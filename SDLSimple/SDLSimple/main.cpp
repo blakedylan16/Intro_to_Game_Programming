f12ac078705f93b68b725dba789169977e4b788c
Dylan Blake <dmb9588@nyu.edu>
2023-10-26 20:13:33 -0400

    Fixed problem with settings

---
+++ SDLSimple/SDLSimple/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDLSimple
+//
+//  Created by Dylan Blake on 10/9/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
